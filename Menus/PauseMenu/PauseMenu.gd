afae0715f0262d6d7cae8efebca43fd97dbf7371
Felipe Guimaraes <guima.felipec@gmail.com>
2018-09-08 21:58:51 -0300

    fixed pause

--- Menus/PauseMenu/PauseMenu.gd
+++ Menus/PauseMenu/PauseMenu.gd
@@ -1,14 +1,19 @@
 extends Control
 
-func _ready():
-	pass
-
-
 func _on_MenuBtn_button_up():
 	get_tree().paused = false
 	SceneLoader.goto_levels_menu()
 
-
 func _on_ResumeBtn_button_up():
 	hide()
 	get_tree().paused = false
+	justpress = false
+
+var justpress = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		if not justpress:
+			justpress = true
+		else:
+			_on_ResumeBtn_button_up()
